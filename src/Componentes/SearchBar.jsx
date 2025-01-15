dbaeacc1fdb9a23791c059878770974e7fe04dbd
Claws35 <mateocuti@gmail.com>
2025-01-15 19:22:32 -0300

    Añade estado de enfoque al componente SearchBar y ajusta su posición en dispositivos móviles

--- src/Componentes/SearchBar.jsx
+++ src/Componentes/SearchBar.jsx
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar = ({
-  searchQuery,
-  setSearchQuery,
-  handleSearch,
-  handleKeyDown,
-  setZoomedIn,
-}) => {
+const SearchBar = ({ searchQuery, setSearchQuery, handleSearch }) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  // Función para verificar si el dispositivo es móvil
+  const isMobile = () => window.innerWidth <= 768;
+
   return (
-    <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-sm mb:p-5 p-3 rounded-full shadow-lg z-50 flex space-x-2 transition-all duration-200 ">
+    <div
+      className={`fixed ${
+        isFocused && isMobile()
+          ? "top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          : "bottom-5 left-1/2 transform -translate-x-1/2"
+      } bg-white bg-opacity-20 backdrop-blur-sm p-3 rounded-full shadow-lg z-50 flex space-x-2 transition-all duration-200`}
+    >
       <input
         type="text"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         onKeyDown={(e) => {
           if (e.key === "Enter") {
             handleSearch();
           }
         }}
-        placeholder="Busca un pais o una historia"
-        className="bg-transparent border border-gray-300 text-white placeholder-gray-400 p-2 rounded-full focus:outline-none w-64 transition-all duration-200 "
+        placeholder="Busca un país o una historia"
+        className="bg-transparent border border-gray-300 text-white placeholder-gray-400 p-2 rounded-full focus:outline-none w-64 transition-all duration-200"
       />
       <button
-        className="bg-[#9b2b70] text-white p-3 rounded-full hover:bg-indigo-600 "
-        onClick={() => {
-          handleSearch();
-        }}
+        className="bg-[#9b2b70] text-white p-3 rounded-full hover:bg-indigo-600"
+        onClick={handleSearch}
       >
         <FaSearch />
       </button>
